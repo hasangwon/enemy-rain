4c3dd29d8f84dbebb27fd462e1c5f17c0d36988f
Sangwon Ha <75872687+hasangwon@users.noreply.github.com>
2022-05-27 09:46:16 +0900

    1

--- enemy.js
+++ enemy.js
@@ -1,3 +1,12 @@
-class Enemy {
+// class Enemy {
   
-}
+// }
+
+const button = document.querySelector("#btnstart");
+
+button.addEventListener('click',function(){
+    button.style.backgroundColor = "yellow";
+    button.style.color = "black";
+    button.innerHTML = "게임중";
+})
+//인터벌 사용해라 ~
--- hero.js
+++ hero.js
@@ -1,3 +1,33 @@
-class Hero {
-  
-}
+// class Hero {
+
+// }
+const man = document.querySelector("#hero");
+const manstyle = document.querySelector('#hero').style;
+let position = 300;
+
+window.addEventListener('keydown', function(event) {    
+
+    if(event.code=="ArrowRight"&& man.offsetLeft <= 560 ){
+        position = position + 8;
+        man.style.left = position + "px";
+    }
+
+    if(event.code=="ArrowLeft"&& man.offsetLeft >= 10){
+        position = position - 8;
+        man.style.left = position + "px";
+    }
+
+    if(event.code=="ArrowRight"){
+        man.style.backgroundPosition = "35px 0px";
+    }
+
+    if(event.code=="ArrowLeft"){
+        man.style.backgroundPosition = "70px 0px";
+    }
+
+});
+
+window.addEventListener('keyup',function(event){
+
+    man.style.backgroundPosition = "0px 0px";
+})
